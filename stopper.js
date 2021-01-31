ecfda03a88c8e1ec99c24688ac882da8861cc117
Jancsi <gulyasjancsi@gmail.com>
2021-01-31 17:09:29 +0100

    Pontosabb mérés, flag-elés

--- stopper.js
+++ stopper.js
@@ -4,67 +4,79 @@ class Stopper {
         this.paused = false;
         this.ido = 0;
         this.timer = null;
+        this.blank = null;
+        this.kezdet = new Date();
+        this.taroltIdo = 0;
         this.start();
     }
-    /*
-    set paused  (paused)  { this._paused = paused               }
-    get paused  ()       { return this._paused                }
-    set ido  (ido)  { this._ido = ido               }
-    get ido  ()       { return this._ido                }*/
 
     start() {
-    var self = this;
-    document.getElementById("controls").innerHTML= `
-    <button id="pause">Pause</button>
-    <button id="stop">Stop</button>
-    `;
-    document.getElementById("pause").onclick = function() { self.pause(); };
-    document.getElementById("stop").onclick = function() { self.stop(); };
+        this.blank = document.getElementById("controls").innerHTML;
+        var self = this;
+        document.getElementById("controls").innerHTML= `
+        <button id="flag">Flag</button>
+        <button id="pause">Pause</button>
+        <button id="stop">Stop</button>
+        `;
+        document.getElementById("flag").onclick = function() { self.flag(); };
+        document.getElementById("pause").onclick = function() { self.pause(); };
+        document.getElementById("stop").onclick = function() { self.stop(); };
 
-    this.timer = setInterval((function(scope) {return function() {
-        scope.update(scope);};})(this), 10); //10 az egy századmásodperc
+        this.timer = setInterval((function(scope) {return function() {
+            scope.update(scope);};})(this), 10); //10 az egy századmásodperc
 
+    }
+
+    flag(){
+        let eddigi = document.getElementById("saves").innerHTML;
+        document.getElementById("saves").innerHTML = eddigi + "<br>" + this.szmpToDigital(this.ido);
     }
 
     pause(){
-        if(this.paused){
+        if(this.paused){ //folytatás pausebol
+            this.kezdet = new Date();
             this.paused = false;
-        }else{
+        }else{ //pause-olás
+            this.taroltIdo = this.ido;
             this.paused = true;
         }
-        
     }
     
     stop(){
         clearInterval(this.timer); 
-        console.log(this.timer);
         document.getElementById("ido").innerHTML = "00:00:00";
-        //vissza kéne állítani a controlst
+        
+        document.getElementById("controls").innerHTML= this.blank; //controls alaphelyzetbe
+        document.getElementById("saves").innerHTML= "";
     }
 
     update(scope){
         if(!scope.paused){
-            scope.ido++; //talán pontosabb lenne ha csak lekérem az időt
-            let perc = Math.floor(scope.ido/100/60).toLocaleString('en-US', { //ezzel elérem hogy szép 00 alakú legyen
-                minimumIntegerDigits: 2,
-                useGrouping: false
-            });
-            let mp = Math.floor(scope.ido/100%60).toLocaleString('en-US', {
-                minimumIntegerDigits: 2,
-                useGrouping: false
-            });
-            let szmp = (scope.ido % 100).toLocaleString('en-US', {
-                minimumIntegerDigits: 2,
-                useGrouping: false
-            });
-
-            let textido = perc + ":" + mp + ":" + szmp;
+            scope.jelen = new Date();
+            scope.ido = (scope.jelen - scope.kezdet); //ez milisecet ad, sokkal pontosabban mint ha incrementálnám az idot
+            scope.ido = Math.floor(scope.ido/10) + scope.taroltIdo; //átváltom mert eddig szmp-ben dolgoztam
+            let textido = scope.szmpToDigital(scope.ido);
             document.getElementById("ido").innerHTML = textido;
             }
+    }
+
+    szmpToDigital(osszesSzmp){
+        let perc = Math.floor(osszesSzmp/100/60).toLocaleString('en-US', { //ezzel elérem hogy szép 00 alakú legyen
+            minimumIntegerDigits: 2,
+            useGrouping: false
+        });
+        let mp = Math.floor(osszesSzmp/100%60).toLocaleString('en-US', {
+            minimumIntegerDigits: 2,
+            useGrouping: false
+        });
+        let szmp = (osszesSzmp % 100).toLocaleString('en-US', {
+            minimumIntegerDigits: 2,
+            useGrouping: false
+        });
+        return perc + ":" + mp + ":" + szmp;
     }
 }
 
 function create(){
     var mainStopper = new Stopper();
-    
 }
